384ea56f83ff5e31d80a21c4fb4f6007fe3b5580
darioorg10 <dricgar1103@g.educaand.es>
2023-06-09 11:14:02 +0200

    Casi terminado, falta la matriz de mayor a menor

--- src/main/java/ejercicio/Aplicacion.java
+++ src/main/java/ejercicio/Aplicacion.java
@@ -5,15 +5,24 @@
 package ejercicio;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,9 +31,10 @@ import java.util.Scanner;
 public class Aplicacion {
     
     public static void main(String[] args) {
-               
+
         Scanner teclado = new Scanner(System.in);
         String menu = """
+                      
                                   ¿Qué quieres hacer?
                                    
                                    1. Leer el archivo, cargar el contenido en una matriz e imprimirla
@@ -33,66 +43,108 @@ public class Aplicacion {
                                    4. Leer el archivo, cargar el contenido en una matriz y ordenar cada fila de mayor a menor e imprimirla
                                    5. Salir 
                                    """;
-        
+
         int respuesta;
-        
-        do {            
-            System.out.println(menu);        
+
+        do {
+            System.out.println(menu);
             respuesta = teclado.nextInt();
-            
-            
+
             switch (respuesta) {
-                  case 1 -> {
+                case 1 -> {
+
+                    int[][] matriz = leerFichero1("./matrizP(1).txt");
+                    imprimirMatriz(matriz);
+
+                }
+                case 2 -> {
+                    
+                    int[][] matriz = leerFichero1("./matrizP(1).txt");
+                    int[][] inversa = matrizInversa(matriz);
+                    imprimirMatriz(inversa);
                       
-                           int[][]matriz = leerFichero1("./matrizP(1).txt");
-                           imprimirMatriz(matriz);                      
-                      
                 }
-                  case 2 -> {
+                case 3 -> {
+                    
+                    int[][] matriz = leerFichero1("./matrizP(1).txt");
+                    int[][] inversa = matrizInversa(matriz);                    
+                    String ruta = JOptionPane.showInputDialog("¿Cómo quieres llamar al fichero?");
+                    escribirInversa(inversa, ruta + ".txt");
+                    
                 }
-                  case 3 -> {
+                case 4 -> {
+                    
+                    int[][] matriz = leerFichero1("./matrizP(1).txt");
+                    for (int i = 0; i < matriz.length; i++) {
+                        Arrays.sort(matriz[i]);
+                    }
+                    
+                    int[][] ordenada = mayorAMenor(matriz);
+                    imprimirMatriz(ordenada);
+                    
                 }
-                  case 4 -> {
+                case 5 -> {
+                    break;
                 }
-                  case 5 -> {
-                }
-                default -> throw new AssertionError();
+                default ->
+                    throw new AssertionError();
             }
-            
-            
-            
-            
-            
-            
+
         } while (respuesta != 5);
+
+    }
+    
+    // Métodos        
+    
+    // Método para contar las líneas de la matriz
+    private static List<String> lineasMatriz(String ruta){
+            
+        List<String> lineas = null;
         
+        try {
+            lineas = Files.readAllLines(Path.of(ruta));            
+        } catch (IOException ex) {
+            Logger.getLogger(Aplicacion.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        
-        
+        return lineas;
         
     }
     
-    // Métodos
+    // Método para imprimir una matriz
+    private static void imprimirMatriz(int[][] matriz){
     
-    // Método para leer el fichero
-    private static int[][] leerFichero1(String ruta) {        
-        int[][] matriz = new int[][];
-        String linea = "";
-        String[] tokens;
+         for (int i = 0; i < matriz.length; i++) {
+             for (int j = 0; j < matriz[i].length; j++) {
+                 System.out.print(matriz[i][j] + " ");                 
+             }
+             System.out.println("");
+        }
+    
+    }
+    
+    // Método para leer el fichero (caso 1)
+    private static int[][] leerFichero1(String ruta) {
+        List<String> lineas = lineasMatriz(ruta);
+        int[][] matriz = new int[lineas.size()][];
 
         try ( Scanner flujo = new Scanner(new FileReader(ruta))) {
-            
-            while (flujo.hasNext()) {                
-                linea = flujo.nextLine();
-                tokens = linea.split(",");                                  
-                
-                for (int i = 0; i < matriz.length; i++) {                    
-                    matriz[i] = linea.length();
+
+            while (flujo.hasNext()) {
+
+                for (int i = 0; i < matriz.length; i++) {
+                    String linea = "";
+                    String[] tokens;
+                    linea = flujo.nextLine();
+                    tokens = linea.split(",");
+                    matriz[i] = new int[tokens.length];
+
                     for (int j = 0; j < matriz[i].length; j++) {
-                        
+                        matriz[i][j] = Integer.parseInt(tokens[j]);
                     }
+
                 }
-                                                
+
             }
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
@@ -101,15 +153,68 @@ public class Aplicacion {
         return matriz;
     }
     
-    // Método para imprimir una matriz
-    private static void imprimirMatriz(int[][] matriz){
-    
-         for (int i = 0; i < matriz.length; i++) {
-             for (int j = 0; j < matriz[i].length; j++) {
-                 System.out.println(matriz[i][j] + " ");
-             }
-             System.out.println("");
+    // Método para hacer la inversa (caso 2)
+    private static int[][] matrizInversa(int [][] matriz){
+        
+        int[][] inversa = new int[matriz.length][];
+        int contador = 0;
+        
+        // Recorrer la i descendentemente y la j ascendentemente
+        for (int i = matriz.length-1; i >= 0; i--) {
+            inversa[contador] = new int[matriz[i].length];
+            
+            for (int j = 0; j < matriz[i].length; j++) {
+                inversa[contador][j] = matriz[i][j];
+            }
+            contador++;
         }
+        
+        
+        return inversa;
+    
+    }    
+    
+    // Método para escribir en un fichero de texto la matriz invertida (caso 3)
+    private static void escribirInversa(int[][] matriz, String ruta){
+    
+        String texto = "";
+
+        try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(ruta))) {
+
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    texto = String.valueOf(matriz[i][j]) + ","; // String textoModificado = texto.substring(0, texto.length()-1); (si no fuera en bucles, así se podría quitar la coma)                    
+                    flujo.write(texto);                  
+                }
+                flujo.newLine(); // Cada vez que termina una fila hacemos una nueva línea
+            }           
+
+            System.out.println("El archivo " + ruta + " ha sido creado satisfactoriamente");
+        } catch (IOException ioe) {
+            System.out.println("No se ha podido crear el fichero correctamente");
+        }
+        
+    }
+    
+    
+    // Método para ordenar cada fila de la matriz de mayor a menor (caso 4)
+    private static int[][] mayorAMenor(int[][] matriz){
+        
+        int[][]ordenada = new int[matriz.length][];
+        int contador = 0;
+        
+        for (int i = 0; i < matriz.length; i++) {
+            ordenada[contador] = new int[matriz[i].length];
+            for (int j = 0; j < matriz[i].length; j--) {
+                ordenada[contador][j] = matriz[i][j];
+                contador++;
+            }
+            contador = 0;
+        }
+        
+        
+        
+        return ordenada;
     
     }
     
